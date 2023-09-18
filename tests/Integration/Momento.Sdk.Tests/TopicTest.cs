3df9a2f298fd0d7e0e77b0801aa3a5b590d0b619
Nate Anderson <nate@momentohq.com>
2023-09-18 10:03:28 -0700

    Add teh cache fixture back in.

--- tests/Integration/Momento.Sdk.Tests/TopicTest.cs
+++ tests/Integration/Momento.Sdk.Tests/TopicTest.cs
@@ -1,13 +1,22 @@
 #if NET6_0_OR_GREATER
 
+using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 
 // ReSharper disable once CheckNamespace
 namespace Momento.Sdk.Tests;
 
-public class TopicTest
+public class TopicTest : IClassFixture<CacheClientFixture>
 {
-    [Fact]
+    private readonly string cacheName;
+
+    public TopicTest(CacheClientFixture cacheFixture)
+    {
+        cacheName = cacheFixture.CacheName;
+    }
+
+    [Fact(Timeout = 5000)]
     public async Task PublishAndSubscribe_String_Succeeds()
     {
         await Task.Run(() => Task.Delay(10000));
