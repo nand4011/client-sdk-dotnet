f0308aee8e2db3828effc5ca1309113cb2affc7c
Nate Anderson <nate@momentohq.com>
2023-09-18 09:16:36 -0700

    Disable all topic tests but one.

--- tests/Integration/Momento.Sdk.Tests/TopicTest.cs
+++ tests/Integration/Momento.Sdk.Tests/TopicTest.cs
@@ -1,7 +1,6 @@
 #if NET6_0_OR_GREATER
 
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -17,102 +16,6 @@ public class TopicTest : IClassFixture<CacheClientFixture>, IClassFixture<TopicC
     {
         topicClient = topicFixture.Client;
         cacheName = cacheFixture.CacheName;
-    }
-
-    [Theory]
-    [InlineData(null, "topic")]
-    [InlineData("cache", null)]
-    public async Task PublishAsync_NullChecksByteArray_IsError(string badCacheName, string badTopicName)
-    {
-        var response = await topicClient.PublishAsync(badCacheName, badTopicName, Array.Empty<byte>());
-        Assert.True(response is TopicPublishResponse.Error, $"Unexpected response: {response}");
-        Assert.Equal(MomentoErrorCode.INVALID_ARGUMENT_ERROR, ((TopicPublishResponse.Error)response).ErrorCode);
-    }
-
-    [Fact]
-    public async Task PublishAsync_PublishNullByteArray_IsError()
-    {
-        var response = await topicClient.PublishAsync(cacheName, "topic", (byte[])null!);
-        Assert.True(response is TopicPublishResponse.Error, $"Unexpected response: {response}");
-        Assert.Equal(MomentoErrorCode.INVALID_ARGUMENT_ERROR, ((TopicPublishResponse.Error)response).ErrorCode);
-    }
-
-    [Fact]
-    public async Task PublishAsync_BadCacheNameByteArray_IsError()
-    {
-        var response = await topicClient.PublishAsync("fake-" + cacheName, "topic", Array.Empty<byte>());
-        Assert.True(response is TopicPublishResponse.Error, $"Unexpected response: {response}");
-        Assert.Equal(MomentoErrorCode.NOT_FOUND_ERROR, ((TopicPublishResponse.Error)response).ErrorCode);
-    }
-
-    [Theory]
-    [InlineData(null, "topic")]
-    [InlineData("cache", null)]
-    public async Task PublishAsync_NullChecksString_IsError(string badCacheName, string badTopicName)
-    {
-        var response = await topicClient.PublishAsync(badCacheName, badTopicName, "value");
-        Assert.True(response is TopicPublishResponse.Error, $"Unexpected response: {response}");
-        Assert.Equal(MomentoErrorCode.INVALID_ARGUMENT_ERROR, ((TopicPublishResponse.Error)response).ErrorCode);
-    }
-
-    [Fact]
-    public async Task PublishAsync_PublishNullString_IsError()
-    {
-        var response = await topicClient.PublishAsync(cacheName, "topic", (string)null!);
-        Assert.True(response is TopicPublishResponse.Error, $"Unexpected response: {response}");
-        Assert.Equal(MomentoErrorCode.INVALID_ARGUMENT_ERROR, ((TopicPublishResponse.Error)response).ErrorCode);
-    }
-
-    [Fact]
-    public async Task PublishAsync_BadCacheNameString_IsError()
-    {
-        var response = await topicClient.PublishAsync("fake-" + cacheName, "topic", "value");
-        Assert.True(response is TopicPublishResponse.Error, $"Unexpected response: {response}");
-        Assert.Equal(MomentoErrorCode.NOT_FOUND_ERROR, ((TopicPublishResponse.Error)response).ErrorCode);
-    }
-
-    [Theory]
-    [InlineData(null, "topic")]
-    [InlineData("cache", null)]
-    public async Task SubscribeAsync_NullChecks_IsError(string badCacheName, string badTopicName)
-    {
-        var response = await topicClient.SubscribeAsync(badCacheName, badTopicName);
-        Assert.True(response is TopicSubscribeResponse.Error, $"Unexpected response: {response}");
-        Assert.Equal(MomentoErrorCode.INVALID_ARGUMENT_ERROR, ((TopicSubscribeResponse.Error)response).ErrorCode);
-    }
-
-    [Fact(Timeout = 5000)]
-    public async Task PublishAndSubscribe_ByteArray_Succeeds()
-    {
-        const string topicName = "topic_bytes";
-        var valuesToSend = new List<byte[]>
-        {
-            new byte[] { 0x00 },
-            new byte[] { 0x01 },
-            new byte[] { 0x02 },
-            new byte[] { 0x03 },
-            new byte[] { 0x04 },
-            new byte[] { 0x05 }
-        };
-
-        var produceCancellation = new CancellationTokenSource();
-        produceCancellation.CancelAfter(2000);
-
-        // we don't need to put this on a different thread
-        var consumeTask = ConsumeMessages(topicName, produceCancellation.Token);
-        await Task.Delay(500);
-
-        await ProduceMessages(topicName, valuesToSend);
-        await Task.Delay(500);
-
-        produceCancellation.Cancel();
-
-        var consumedMessages = await consumeTask;
-        Assert.Equal(valuesToSend.Count, consumedMessages.Count);
-        for (var i = 0; i < valuesToSend.Count; ++i)
-        {
-            Assert.Equal(((TopicMessage.Binary)consumedMessages[i]).Value, valuesToSend[i]);
-        }
     }
 
     [Fact(Timeout = 5000)]
@@ -145,22 +48,6 @@ public class TopicTest : IClassFixture<CacheClientFixture>, IClassFixture<TopicC
         for (var i = 0; i < valuesToSend.Count; ++i)
         {
             Assert.Equal(((TopicMessage.Text)consumedMessages[i]).Value, valuesToSend[i]);
-        }
-    }
-
-    private async Task ProduceMessages(string topicName, List<byte[]> valuesToSend)
-    {
-        foreach (var value in valuesToSend)
-        {
-            var publishResponse = await topicClient.PublishAsync(cacheName, topicName, value);
-            switch (publishResponse)
-            {
-                case TopicPublishResponse.Success:
-                    await Task.Delay(100);
-                    break;
-                default:
-                    throw new Exception("publish error");
-            }
         }
     }
 
@@ -206,75 +93,6 @@ public class TopicTest : IClassFixture<CacheClientFixture>, IClassFixture<TopicC
             default:
                 throw new Exception("subscription error");
         }
-    }
-    
-    [Fact]
-    public async Task MultipleSubscriptions_HappyPath()
-    {
-        var numTopics = 20;
-
-        var subscriptionResponses = await Task.WhenAll(
-            Enumerable.Range(1, numTopics).Select(i =>
-                topicClient.SubscribeAsync(cacheName, $"topic{i}")
-                    .ContinueWith(r => Tuple.Create(i, r.Result))).ToList());
-        
-        var subscriptions = subscriptionResponses.Select(t =>
-        {
-            var (topicNum, subscriptionResponse) = t;
-            if (subscriptionResponse is TopicSubscribeResponse.Subscription subscription)
-            {
-                return Tuple.Create(topicNum, subscription);
-            }
-
-            throw new Exception($"Got an unexpected subscription response: {subscriptionResponse}");
-        }).ToList();
-
-        var subscribers = subscriptions.Select(t => Task.Run(async () =>
-        {
-            var (topicNum, subscription) = t;
- 
-            int messageCount = 0;
-            await foreach (var message in subscription)
-            {
-                switch (message)
-                {
-                    case TopicMessage.Text:
-                        messageCount++;
-                        break;
-                }
-            }
-
-            return messageCount;
-        })).ToList();
-    
-        await Task.Delay(100);
-
-        const int numMessagesToPublish = 50;
-        foreach (var i in Enumerable.Range(0, numMessagesToPublish))
-        {
-            var randomTopic = Random.Shared.NextInt64(numTopics) + 1;
-            var messageId = $"message{i}";
-            var topic = $"topic{randomTopic}";
-            var publishResponse = await topicClient.PublishAsync(cacheName, topic, messageId);
-            if (publishResponse is not TopicPublishResponse.Success)
-            {
-                throw new Exception($"Publish did not succeed: {publishResponse}");
-            }
-
-            await Task.Delay(100);
-        }
-    
-        await Task.Delay(1_000);
-        
-        foreach (var subscriptionTuple in subscriptions)
-        {
-            var (_, subscription) = subscriptionTuple;
-            subscription.Dispose();
-        }
-    
-        var subscriberResults = await Task.WhenAll(subscribers);
-        var numMessagesReceived = subscriberResults.Sum();
-        Assert.Equal(numMessagesToPublish, numMessagesReceived);
     }
 }
 #endif
